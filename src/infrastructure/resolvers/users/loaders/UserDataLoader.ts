04353e1804ca9b5ef2544ea148f9c63282f8a1b3
Marcos Giordano <marcos_l14@hotmail.com>
2025-10-18 19:39:48 -0300

    Fixed UserDataLoader

--- src/infrastructure/resolvers/users/loaders/UserDataLoader.ts
+++ src/infrastructure/resolvers/users/loaders/UserDataLoader.ts
@@ -5,14 +5,14 @@ import { Service } from "typedi";
 
 @Service()
 export class UserDataLoader {
-  private loader = new DataLoader<number, IUser>(async (userIds) => {
-    const users = await this.getUserListUseCase.execute([...userIds]);
-    return users;
-  });
-
   constructor(private readonly getUserListUseCase: GetUserListUseCase) {}
 
   public async load(id: number): Promise<IUser> {
-    return await this.loader.load(id);
+    const loader = new DataLoader<number, IUser>(async (userIds) => {
+      const users = await this.getUserListUseCase.execute([...userIds]);
+      return users;
+    });
+
+    return await loader.load(id);
   }
 }
