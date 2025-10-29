8d50242c8f8a2503e68297c6a77235b2024a140d
Marcos Giordano <marcos_l14@hotmail.com>
2025-10-29 11:20:50 -0300

    Fixed Data Loaders to not create a new instance with every load()

--- src/infrastructure/resolvers/bets/loaders/BetDataLoader.ts
+++ src/infrastructure/resolvers/bets/loaders/BetDataLoader.ts
@@ -5,21 +5,34 @@ import { GetBetListUseCase } from "../../../../application/usecases/bets/GetBetL
 
 @Service()
 export class BetDataLoader {
-  constructor(private readonly getBetListUseCase: GetBetListUseCase) {}
+  private loader: DataLoader<number, IBet[]>;
+
+  constructor(private readonly getBetListUseCase: GetBetListUseCase) {
+    this.loader = new DataLoader<number, IBet[]>(
+      async (userIds) => {
+        const foundBets = await this.getBetListUseCase.execute({
+          userIds: [...userIds],
+        });
+
+        const betMap = new Map<number, IBet[]>();
+        for (const bet of foundBets) {
+          const list = betMap.get(bet.userId);
+          if (list) {
+            list.push(bet);
+          } else {
+            betMap.set(bet.userId, [bet]);
+          }
+        }
+
+        return userIds.map((userId) => betMap.get(userId) ?? []);
+      },
+      {
+        cache: false,
+      }
+    );
+  }
 
   public async load(userId: number): Promise<IBet[]> {
-    const loader = new DataLoader<number, IBet[]>(async (userIds) => {
-      const allBets = await this.getBetListUseCase.execute({
-        userIds: [...userIds],
-      });
-
-      const betsByUserId = userIds.map((userId) =>
-        allBets.filter((bet) => bet.userId === userId)
-      );
-
-      return betsByUserId;
-    });
-
-    return await loader.load(userId);
+    return await this.loader.load(userId);
   }
 }
