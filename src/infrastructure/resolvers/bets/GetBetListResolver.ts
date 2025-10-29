fa6782fbb00388971354ff6bf37eb15591be10d5
Marcos Giordano <marcos_l14@hotmail.com>
2025-10-29 11:19:58 -0300

    Added data types validations

--- src/infrastructure/resolvers/bets/GetBetListResolver.ts
+++ src/infrastructure/resolvers/bets/GetBetListResolver.ts
@@ -4,6 +4,7 @@ import { Bet } from "../../../application/models/Bet";
 import { IBet } from "../../../domain/entities/IBet";
 import { GetBetListUseCase } from "../../../application/usecases/bets/GetBetListUseCase";
 import { GraphQlErrorHandling } from "../errors/GraphQlErrorHandling";
+import { ValidationError } from "../../../application/erros/ValidationError";
 
 @Service()
 @Resolver(() => Bet)
@@ -19,12 +20,27 @@ export class GetBetListResolver {
       if (!page) page = 1;
       if (!limit) limit = 100;
 
+      this.validatePaginationParams(page, limit);
+
       return await this.getBetListUseCase.execute({
         page,
         limit,
       });
     } catch (error) {
       throw GraphQlErrorHandling.handle(error as Error);
+    }
+  }
+
+  private validatePaginationParams(page: number, limit: number): void {
+    if (isNaN(page) || isNaN(limit)) {
+      throw new ValidationError("Page and limit must be numbers.");
+    }
+
+    if (page <= 0) {
+      throw new ValidationError("Page number must be greater than 0.");
+    }
+    if (limit <= 0 || limit > 1000) {
+      throw new ValidationError("Limit must be between 1 and 1000.");
     }
   }
 }
